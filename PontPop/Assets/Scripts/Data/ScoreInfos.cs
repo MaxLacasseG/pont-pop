892b8eea861db4de4f43224f5e0eefdda48e96e7
MaxLacasseG <max.lacasse.g@gmail.com>
2019-12-05 07:40:41 -0500

    Setup data

---
+++ PontPop/Assets/Scripts/Data/ScoreInfos.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreInfos {
+
+    public int userId;
+    public int id;
+    public string title;
+    public bool completed;
+
+}
